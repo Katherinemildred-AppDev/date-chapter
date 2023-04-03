e09d9d284a8605740784bc570fb57046b54b4fe0
Katherine O'Connell <koconnel@chicagobooth.edu>
2023-04-03 01:48:04 +0000

    Date exercise final

--- date_monday.rb
+++ date_monday.rb
@@ -1,10 +1,14 @@
 # Should output:
-# 
+#
 #   "Is today Monday? true"
-# 
+#
 # if today is Monday, or
-# 
+#
 #   "Is today Monday? false"
-# 
+#
 # if today is not Monday.
 
+require ("date")
+a = Date.today
+
+p "Is today Monday? " + a.monday?.to_s
