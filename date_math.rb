3ec9b55f576d4a58036c4f0f1e35aa06e3bced28
Katherine O'Connell <koconnel@chicagobooth.edu>
2023-04-03 01:38:54 +0000

    Dates 2nd task

--- date_math.rb
+++ date_math.rb
@@ -1,7 +1,14 @@
 # Should output the number of days since Ruby was made.
-# 
+#
 # (Ruby released to the public on `December 21, 1995`.)
-# 
+#
 # Output:
 #   "Ruby is 108937 days old!"
 #
+
+require "date"
+t = Date.today
+v = Date.parse("December 21, 1995")
+a = (t - v).round
+a = a.to_s
+p "Ruby is " + a + " days old!"
